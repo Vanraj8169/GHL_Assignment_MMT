8052beaba9fff5b9b197e8dc2a9bb7625285b6cd
Vanraj Pardeshi <87256781+Vanraj8169@users.noreply.github.com>
2024-12-07 20:02:44 +0530

    Update locators.ts

--- src/locators/locators.ts
+++ src/locators/locators.ts
@@ -1,10 +1,24 @@
 export const Locators = {
-    Flight: "(//span[contains(@class,'headerIconTextAlignment chNavText')])[1]",
+    Flight: "//a[normalize-space(text())='Flights']",
     CancelBtn: "//span[@class='commonModal__close']",
-    FromCityInput: "(//input[contains(@class,'fsw_inputField lineHeight36')])[1]",
-    ToCityInput: "(//input[contains(@class,'fsw_inputField lineHeight36')])[2]",
-    FromCityTextField: "//input[@placeholder='From']",
-    ToCityTextField: "//input[@placeholder='To']",
-    CitySelection: "(//img[@alt='icon']/following-sibling::div)[1]",
-    SearchBtn: "//a[contains(@class,'primaryBtn font24')]"
+    FromCityInput: "(//input[@class='autoFlll cityinput'])[1]",
+    ToCityInput: "(//input[@class='srctinput autoFlll'])[2]",
+    FromCityTextField: "(//input[@class='srctinput autoFlll'])[1]",
+    ToCityTextField: "(//input[@class='srctinput autoFlll'])[2]",
+    FromCitySelection: "(//div[@class='mflexcol'])[1]",
+    ToCitySelection: '//*[@id="toautoFill"]/ul/li[1]',
+    prices: "//span[@class='active-date']",
+    SearchBtn: '//*[@id="divSearchFlight"]/button',
+    FlightSearch: {
+        BookNowBtn: "(//button[contains(@class,'btn book-bt-n')])[1]"
+    },
+    PromoCode: {
+        ClearBtn: "(//div[@class='cpn-r']//div)[2]",
+        PromoCodeTextField: "//input[@ng-model='CouponCode']",
+        ApplyBtn: "//div[normalize-space(text())='apply']",
+        PromoMsg: "//p[normalize-space(text())='Invalid Coupon']",
+        ValidPromoMsg: "(//div[contains(@class,'bor mtp10')]//p)[2]"
+    },
+    ContinueBooking: "//div[@id='divContinueReview2']//span[1]",
+    TicketPrice: "(//div[@class='frettlsum']//span)[1]"
 }
